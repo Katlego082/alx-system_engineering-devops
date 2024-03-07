6e09ed613e1e0465f42f47752c7b219f15901dc0
4775559c46dc <katlego.molopa.082@gmail.com>
2024-03-07 09:21:05 -0800

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
-import requests as r
+"""_summary_
+"""
+import requests
 
 
 def top_ten(subreddit):
-    """Print top 10 post given subreddit."""
+    """_summary_"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
-        Gecko/20100101 Firefox/73.0"
-    }
-    param = {
-        "limit": 10
-    }
-    response = r.get(url, headers=headers, params=param, allow_redirects=False)
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(
+        url, headers=headers, params={"limit": 10}, allow_redirects=False
+    )
     if response.status_code == 404:
         print("None")
-        return
-    results = response.json().get("data")
-    [print(top.get("data").get("title")) for top in results.get("children")]
+
+    data = response.json()
+    res = data.get("data")
+    for c in res.get("children"):
+        print(c.get("data").get("title"))
