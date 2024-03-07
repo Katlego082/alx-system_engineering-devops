c3993706a9e1335cd738f0563e9d7cd13fc3dcf2
4775559c46dc <katlego.molopa.082@gmail.com>
2024-03-07 09:14:20 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""_summary_
-"""
-import requests
+
+import requests as r
 
 
 def number_of_subscribers(subreddit):
-    """_summary_"""
-    if subreddit is None or type(subreddit) is not str:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    response = r.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    subs = data.get("data", {}).get("subscribers", 0)
-    return subs
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""_summary_
-"""
-import requests
+
+import requests as r
 
 
 def top_ten(subreddit):
-    """_summary_"""
+    """Print top 10 post given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "Custom User Agent"}
-
-    response = requests.get(
-        url, headers=headers, params={"limit": 10}, allow_redirects=False
-    )
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    param = {
+        "limit": 10
+    }
+    response = r.get(url, headers=headers, params=param, allow_redirects=False)
     if response.status_code == 404:
         print("None")
-
-    data = response.json()
-    res = data.get("data")
-    for c in res.get("children"):
-        print(c.get("data").get("title"))
+        return
+    results = response.json().get("data")
+    [print(top.get("data").get("title")) for top in results.get("children")]
